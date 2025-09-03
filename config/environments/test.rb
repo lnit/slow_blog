b2ecde6be3e62b41638d7be4e4f6ee642963fa59
lnit <6626484+lnit@users.noreply.github.com>
2025-09-03 16:03:40 +0900

    Prosopite導入

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -51,4 +51,9 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
   config.ci_logger.enabled = true
+
+  config.after_initialize do
+    Prosopite.rails_logger = true
+    Prosopite.raise = true
+  end
 end
